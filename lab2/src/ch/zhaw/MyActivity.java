7122d4be2cfa5e97df5feb5b7eee16f56adf82b6
knobli <raffael@santschi.ch>
2013-07-17 22:19:17 +0200

    new language: german, add settings

--- lab2/src/ch/zhaw/MyActivity.java
+++ lab2/src/ch/zhaw/MyActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -31,6 +33,24 @@ public class MyActivity extends Activity
     }
     
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+    	if(item.getItemId() == R.id.action_settings){
+    		Intent intent = new Intent(MyActivity.this, SettingsActivity.class);
+    		startActivity(intent);
+    		return true;
+    	} else {
+    		return super.onMenuItemSelected(featureId, item);
+    	}
+    }
+    
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
     	// TODO Auto-generated method stub
     	super.onActivityResult(requestCode, resultCode, data);
@@ -45,6 +65,7 @@ public class MyActivity extends Activity
 	    				String value = bundle.getString(key);
 	    				((TextView) this.findViewById(Integer.parseInt(key))).setText(value);
 	    			}
+	    			//String text = String.format(res.getString(R.string.welcome_messages),username, mailCount);
 	    			msg = res.getQuantityString(R.plurals.save_data, numberOfChanges, numberOfChanges);
     			} else {
     				msg = res.getString(R.string.no_save_data);
