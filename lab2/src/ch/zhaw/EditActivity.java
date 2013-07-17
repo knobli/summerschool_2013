17f43195aca9af6634416ed662e86f3ffef46437
knobli <raffael@santschi.ch>
2013-07-17 15:52:49 +0200

    add new code

--- lab2/src/ch/zhaw/EditActivity.java
+++ lab2/src/ch/zhaw/EditActivity.java
@@ -1,7 +1,45 @@
 package ch.zhaw;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EditActivity extends Activity {
 
+	private EditText name;
+	private EditText address;
+	private EditText state;
+	private EditText country;
+	private EditText phone;
+	private Button doneButton;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit);
+        
+        this.name = (EditText) this.findViewById(R.id.textView1);
+        this.address = (EditText) this.findViewById(R.id.textView1);
+        this.state = (EditText) this.findViewById(R.id.textView1);
+        this.country = (EditText) this.findViewById(R.id.textView1);
+        this.phone = (EditText) this.findViewById(R.id.textView1);
+        
+        this.doneButton = (Button) this.findViewById(R.id.editButton);
+        
+        this.doneButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.putExtra("name", name.getText().toString());
+				setResult(Activity.RESULT_OK, intent);
+				finished();
+			}
+		})
+    }
+	
 }
--- lab2/src/ch/zhaw/MyActivity.java
+++ lab2/src/ch/zhaw/MyActivity.java
@@ -1,15 +1,43 @@
 package ch.zhaw;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MyActivity extends Activity
 {
-    /** Called when the activity is first created. */
+    protected static final int REQUEST_TEXT = 0;
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button editButton = (Button) this.findViewById(R.id.editButton);
+        editButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MyActivity.this, EditActivity.class);
+				startActivityForResult(intent, REQUEST_TEXT);
+			}
+		});
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	// TODO Auto-generated method stub
+    	super.onActivityResult(requestCode, resultCode, data);
+    	String msg = "blblllala";
+    	if(requestCode == REQUEST_TEXT){
+    		if(resultCode == Activity.RESULT_OK){
+    			msg = data.getExtras().getString("balbal");
+    		}
+    	}
+    	Toast.makeText(this,msg,Toast.LENGTH_LONG).show();
     }
 }
